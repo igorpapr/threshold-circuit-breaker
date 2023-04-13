e103d4a8d3e6f6af5c724f735feeb999d5cd7964
igorpapr <igorpapr@gmail.com>
2023-04-13 18:13:36 +0300

    Initial commit

---
+++ src/main/java/com/paprotskyi/probabilitycircuitbreaker/ProbabilityCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.paprotskyi.probabilitycircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbabilityCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbabilityCircuitBreakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paprotskyi/probabilitycircuitbreaker/ProbabilityCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paprotskyi.probabilitycircuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbabilityCircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
